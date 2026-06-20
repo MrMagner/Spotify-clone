41173070d1ed96f84d7bc3af4ddf41b94f541a4e
Magner <erfanshaik84@gmail.com>
2026-06-20 06:48:49 +0530

    1:56:57

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("/%5Csongs%5C")[1])
         }
     }
     return songs
@@ -21,5 +21,18 @@ async function main(){
 
     let songs= await getSongs()
     console.log(songs)
+
+    let songUl=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUl.innerHTML=songUl.innerHTML+`<li> ${song.replaceAll("%20"," ")} </li>`
+    }
+
+    var audio=new Audio(songs[0])
+    // audio.play()
+
+    audio.addEventListener("loadeddata",()=>{
+        let duration=audio.duration
+        console.log(duration)
+    })
 }
 main()
